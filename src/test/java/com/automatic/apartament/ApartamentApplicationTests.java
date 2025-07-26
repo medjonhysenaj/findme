44cb6aa6f7db3ca3bd3c4f36e64ead2afa5a5da1
medjonhysenaj <medjon.hysenaj@unishk.edu.al>
2025-07-26 22:28:57 +0200

    Update ApartamentApplicationTests.java

--- src/test/java/com/automatic/apartament/ApartamentApplicationTests.java
+++ src/test/java/com/automatic/apartament/ApartamentApplicationTests.java
@@ -11,3 +11,13 @@ class ApartamentApplicationTests {
 	}
 
 }
+@Component
+public class StartupCheck {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @PostConstruct
+    public void printUrl() {
+        System.out.println("✅ DB URL in use: " + dbUrl);
+    }
+}
